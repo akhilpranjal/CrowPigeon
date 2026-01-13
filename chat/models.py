667e7223b28dd4262c6464aeb6cc87cfe58f332b
Pranjal Srivastava <akhilpranjal11@gmail.com>
2026-01-13 20:47:51 +0530

    chore: redefine the classes/models

--- chat/models.py
+++ chat/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
