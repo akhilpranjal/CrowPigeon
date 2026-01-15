2d2ad7bdd4e89b1a4874c5fe61f4f82ecd5cb9d8
Pranjal Srivastava <akhilpranjal11@gmail.com>
2026-01-15 18:54:10 +0530

    chore: update the Room db

--- chat/models.py
+++ chat/models.py
@@ -4,7 +4,9 @@ from datetime import datetime
 # Create your models here.
 
 class Room(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+    owner_session = models.CharField(max_length=255)
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
