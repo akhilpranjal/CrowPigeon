9671aed14b05e9281087cd1dca2caa64269c987a
Pranjal Srivastava <akhilpranjal11@gmail.com>
2026-01-21 14:18:28 +0530

    chore: add paths for waiting, inbox, approve, reject routes

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/', views.room, name='room'),
+    path('waiting/', views.waiting, name='waiting'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('approve/<int:member_id>/', views.approve, name='approve'),
+    path('reject/<int:member_id>/', views.reject, name='reject'),
 ]
