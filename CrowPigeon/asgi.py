2752b6afa62d1fb68f8e9d849a0d7c314cd57211
Pranjal Srivastava <akhilpranjal11@gmail.com>
2026-03-06 00:46:39 +0530

    import chat.routing after app registry

--- CrowPigeon/asgi.py
+++ CrowPigeon/asgi.py
@@ -5,11 +5,12 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.sessions import SessionMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrowPigeon.settings')
 
 django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
@@ -17,7 +18,7 @@ application = ProtocolTypeRouter({
         SessionMiddlewareStack(
             AuthMiddlewareStack(
                 URLRouter(
-                    chat.routing.websocket_urlpatterns
+                    websocket_urlpatterns
                 )
             )
         )
