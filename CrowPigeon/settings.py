8603cad5bd76646aa53bdeb4d36b2b21bc355c56
Pranjal Srivastava <akhilpranjal11@gmail.com>
2026-03-06 00:40:55 +0530

    update DATABASES to default

--- CrowPigeon/settings.py
+++ CrowPigeon/settings.py
@@ -68,11 +68,10 @@ else:
     }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 LANGUAGE_CODE = 'en-us'
